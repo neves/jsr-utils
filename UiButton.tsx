b47f5e12de208166ee51e0d630a99163c883e75e
Marcos Neves <neves64@icloud.com>
2025-03-06 18:10:28 -0300

    test jsx

--- UiButton.tsx
+++ UiButton.tsx
@@ -1,6 +1,9 @@
-// jsr ainda não suporta jsx: https://github.com/jsr-io/jsr/issues/24
-import type { JSX } from "@bossley9/sjsx/jsx-runtime";
+import {type JSX} from "@bossley9/sjsx/jsx-runtime";
 
 export function UiButton({label, onClick}: {label: string, onClick: string}): JSX.Element {
   return <button onClick={onClick}>{label}</button>
+}
+
+export function Teste() {
+  return <UiButton label="Teste" onClick="play" />
 }
