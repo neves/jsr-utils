8902ba3a91ba4d6541e25ad172bed6a6ba7b6f78
Marcos Neves <neves64@icloud.com>
2025-03-06 18:28:33 -0300

    fix publish errors

--- UiButton.tsx
+++ UiButton.tsx
@@ -1,9 +1,9 @@
-import {type JSX} from "@bossley9/sjsx/jsx-runtime";
+import {type JSX} from "@bossley9/sjsx/jsx-runtime"
 
 export function UiButton({label, onClick}: {label: string, onClick: string}): JSX.Element {
   return <button onClick={onClick}>{label}</button>
 }
 
-export function Teste() {
+export function Teste(): JSX.Element {
   return <UiButton label="Teste" onClick="play" />
 }
